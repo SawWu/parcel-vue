cb4d22391096287e5cebd1eafccf8856aeac72d8
Liu Ren <liuren@cc1st.com>
2018-07-19 21:15:55 +0800

    初始化框架

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    created() {this.$nextTick
+      this.$nextTick(()=>{
+        console.log('张超是傻逼')
+      })
+      console.log(this)
+    },
+  }
+</script>
+
+<style scoped>
+  #app {
+    height: 100%;
+  }
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper">
+    <h1>Hello Vue</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+
+  }
+</script>
+
+<style lang="scss" scoped>
+  .wrapper {
+    width: 100%;
+    height: 100%;
+  }
+</style>
